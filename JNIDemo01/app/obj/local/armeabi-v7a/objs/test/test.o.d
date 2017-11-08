4b3ac1a1cf801d02a1166d076118968cdb495460
zhangshuangxue <1334258239@qq.com>
2017-11-08 19:01:18 +0800

    添加JNI 调用第三方.so和.h文件的Demo

---
+++ JNIDemo01/app/obj/local/armeabi-v7a/objs/test/test.o.d
@@ -0,0 +1,5 @@
+/Users/momo/Documents/workspace_AS/JNIDemo01/app/obj/local/armeabi-v7a/objs/test/test.o: \
+  /Users/momo/Documents/workspace_AS/JNIDemo01/app/jni/test.cpp \
+  /Users/momo/Documents/workspace_AS/JNIDemo01/app/jni/lib/include/Algo.h
+
+/Users/momo/Documents/workspace_AS/JNIDemo01/app/jni/lib/include/Algo.h:
